347171ccf5ac5b1c9cec93182711da4fe5a4e77d
ceekem <37778925+ceekem@users.noreply.github.com>
2019-01-26 16:51:18 +0200

    slider
    slider

--- js/carousel.js
+++ js/carousel.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
     $('.customer-logos').slick({
-      slidesToShow: 6,
+      slidesToShow: 1,
       slidesToScroll: 1,
       autoplay: true,
-      autoplaySpeed: 1000,
+      autoplaySpeed: 3000,
       arrows: false,
       dots: false,
       pauseOnHover: true,
       responsive: [{
         breakpoint: 768,
         settings: {
-          slidesToShow: 4
+          slidesToShow: 1
         }
       }, {
         breakpoint: 520,
         settings: {
-          slidesToShow: 3
+          slidesToShow: 1
         }
       }]
     });
