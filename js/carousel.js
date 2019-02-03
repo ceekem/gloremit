5cf2f4dbcadb9b4e386c0d3bbdc0d8642484695f
raymond <raymond.mortu@gmail.com>
2019-02-03 20:39:01 +0200

    update from mac
    carousel and title

--- js/carousel.js
+++ js/carousel.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
       slidesToShow: 1,
       slidesToScroll: 1,
       autoplay: true,
-      autoplaySpeed: 3000,
+      autoplaySpeed: 5200,
       arrows: false,
       dots: false,
       pauseOnHover: true,
